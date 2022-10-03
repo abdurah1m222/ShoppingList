74ecf42ec5d7dac607e01e679014c3968f0de5e4
Abdy <abdy42796@gmail.com>
2022-10-04 00:27:03 +0600

    Initial commit

---
+++ app/src/main/java/com/timesoft/shoppinglist/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.timesoft.shoppinglist
+//Room Persistence Library
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
