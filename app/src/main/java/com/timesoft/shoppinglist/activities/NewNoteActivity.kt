5eb08e11683b33bc8674f32c9963ffdbb953cc18
Abdy <abdy42796@gmail.com>
2022-10-05 15:28:15 +0600

    do save notes

--- app/src/main/java/com/timesoft/shoppinglist/activities/NewNoteActivity.kt
+++ app/src/main/java/com/timesoft/shoppinglist/activities/NewNoteActivity.kt
@@ -1,11 +1,18 @@
 package com.timesoft.shoppinglist.activities
 
+import android.content.Intent
+import android.icu.text.SimpleDateFormat
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.annotation.RequiresApi
 import com.timesoft.shoppinglist.R
 import com.timesoft.shoppinglist.databinding.ActivityNewNoteBinding
+import com.timesoft.shoppinglist.entities.NoteItem
+import com.timesoft.shoppinglist.fragments.NoteFragment
+import java.util.*
 
 class NewNoteActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNewNoteBinding
@@ -24,11 +31,44 @@ class NewNoteActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.id_save) {
-            finish()
+            setMainResult()
         } else if (item.itemId == android.R.id.home) {
             finish()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMainResult() {
+        val i = Intent().apply {
+            putExtra(NoteFragment.NEW_NOTE_KEY, createNewNote())
+        }
+        setResult(RESULT_OK, i)
+        finish()
+    }
+
+    private fun getCurrentTime(): String {
+        val formatter = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            SimpleDateFormat("hh:mm:ss - yyyy/MM/dd", Locale.getDefault())
+        } else {
+            TODO("VERSION.SDK_INT < N")
+        }
+        return formatter.format(Calendar.getInstance().time)
+    }
+
+    /*@RequiresApi(Build.VERSION_CODES.N)
+    private fun getCurrentTime(): String {
+        val formatter = SimpleDateFormat("hh:mm:ss - yyyy/MM/dd", Locale.getDefault())
+        return formatter.format(Calendar.getInstance().time)
+    }*/
+
+    private fun createNewNote(): NoteItem {
+        return NoteItem(
+            null,
+            binding.edTitle.text.toString(),
+            binding.edDescription.text.toString(),
+            getCurrentTime(),
+            ""
+        )
     }
 
     private fun actionBarSettings() {
--- app/src/main/java/com/timesoft/shoppinglist/fragments/NoteFragment.kt
+++ app/src/main/java/com/timesoft/shoppinglist/fragments/NoteFragment.kt
@@ -1,29 +1,40 @@
 package com.timesoft.shoppinglist.fragments
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.timesoft.shoppinglist.activities.MainApp
 import com.timesoft.shoppinglist.activities.NewNoteActivity
+import com.timesoft.shoppinglist.adapters.NoteAdapter
 import com.timesoft.shoppinglist.databinding.FragmentNoteBinding
 import com.timesoft.shoppinglist.db.MainViewModel
+import com.timesoft.shoppinglist.entities.NoteItem
 
 class NoteFragment : BaseFragment() {
     private lateinit var binding: FragmentNoteBinding
+    private lateinit var editLauncher: ActivityResultLauncher<Intent>
+    private lateinit var adapter: NoteAdapter
+
     private val mainViewModel: MainViewModel by activityViewModels {
         MainViewModel.MainViewModelFactory((context
             ?.applicationContext as MainApp).database)
     }
 
     override fun onClickNew() {
-        startActivity(Intent(activity, NewNoteActivity::class.java))
+        editLauncher.launch(Intent(activity, NewNoteActivity::class.java))
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        onEditResult()
     }
 
     override fun onCreateView(
@@ -34,7 +45,35 @@ class NoteFragment : BaseFragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRcView()
+        observer()
+    }
+
+    private fun initRcView() = with(binding) {
+        rcViewNote.layoutManager = LinearLayoutManager(activity)
+        adapter = NoteAdapter()
+        rcViewNote.adapter = adapter
+    }
+
+    private fun observer() {
+        mainViewModel.allNotes.observe(viewLifecycleOwner) {
+            adapter.submitList(it)
+        }
+    }
+
+    private fun onEditResult() {
+        editLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                mainViewModel.insertNote(it.data?.getSerializableExtra(NEW_NOTE_KEY) as NoteItem)
+            }
+        }
+    }
+
     companion object {
+        const val NEW_NOTE_KEY = "title_key"
         @JvmStatic
         fun newInstance() = NoteFragment()
     }
